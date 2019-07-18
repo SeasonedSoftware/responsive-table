ef8a4145735c2ef2df4606d179119f11f0d54a91
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-17 22:33:46 -0300

    Updates

---
+++ precache-manifest.e3da0c90fd6e87d2b4f7e816d89c729f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30701677c20b0cd166fdc6ed3f3e9734",
+    "url": "/responsive-table/index.html"
+  },
+  {
+    "revision": "bc6e3c9f4c8ac3a8c48e",
+    "url": "/responsive-table/static/css/main.4541ae13.chunk.css"
+  },
+  {
+    "revision": "d52adef07340b80c885b",
+    "url": "/responsive-table/static/js/2.c1728619.chunk.js"
+  },
+  {
+    "revision": "bc6e3c9f4c8ac3a8c48e",
+    "url": "/responsive-table/static/js/main.1c9feec8.chunk.js"
+  },
+  {
+    "revision": "980c28a3dff46c2c2cdf",
+    "url": "/responsive-table/static/js/runtime~main.83028fe1.js"
+  }
+]);
